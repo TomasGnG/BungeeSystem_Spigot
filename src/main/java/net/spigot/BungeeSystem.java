e2f49a6a3c5c6205e1f573231c2000eaa7afe333
Tomas <tomaskeder2005@gmail.com>
2021-03-14 14:36:35 +0100

    Methods created

--- src/main/java/net/spigot/BungeeSystem.java
+++ src/main/java/net/spigot/BungeeSystem.java
@@ -1,4 +1,21 @@
 package net.spigot;
 
-public class BungeeSystem extends Java{
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.api.plugin.PluginManager;
+
+public class BungeeSystem extends Plugin {
+
+    private static BungeeSystem instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        registerEverything();
+    }
+
+    public void registerEverything() {
+        PluginManager manager = ProxyServer.getInstance().getPluginManager();
+    }
 }
